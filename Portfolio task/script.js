4b524286c98760ecbca06704df5c801a2e05dbb0
unknown <jeniferrefinej0@gmail.com>
2024-08-21 11:27:33 +0530

    Task-1 portfolio

---
+++ Portfolio task/script.js
@@ -0,0 +1,6 @@
+const viewcode = document.getElementById("view--code--button")
+const viewProject = document.getElementById("third--project--container")
+viewcode.addEventListener("click",function(){
+    viewProject.style.display = "block"
+    viewcode.style.display="none"
+})
